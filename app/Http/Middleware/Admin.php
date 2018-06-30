4d360e033e4acbc433854322e2da130d6a4e00b7
Ismael Lastre ALvarez <ismaelfac@gmail.com>
2018-06-29 22:05:31 -0500

    conditional if by case in Admin middleware

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -15,11 +15,19 @@ class Admin
      */
     public function handle($request, Closure $next)
     {
-        if(! auth()->user()->is_admin){
-            return redirect('/');
-        }else{
-            return response()->view('admin.content');
+        switch (auth()->user()) 
+        {
+            case 'is_admin':
+                return response()->view('admin.content');
+                break;
+            default:
+                return redirect('/'); 
         }
+        // if(! auth()->user()->is_admin){
+        //     return redirect('/');
+        // }else{
+        //     return response()->view('admin.content');
+        // }
         return $next($request);
     }
 }
